1a17c4c601eb381a30c7221dac6ecdcd63618886
Ibrahim <horlalekan123@gmail.com>
2021-08-23 17:05:14 +0100

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const fetch = require("node-fetch");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("We are live");
 });
