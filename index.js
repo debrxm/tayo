e17f9dfe8186663822a117914c0f22f09558ed02
Ibrahim <horlalekan123@gmail.com>
2021-08-23 17:13:57 +0100

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 //mailchimp code:
 const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const path = require("path");
 const fetch = require("node-fetch");
-const port = process.env.PORT || 5000;
+const { json } = require("express");
+dotenv.config({
+  path: "./config.env",
+});
+
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
 app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("We are live");
 });
@@ -47,4 +49,6 @@ app.post("/signup", (req, res) => {
 
 app.use(express.static(path.join(__dirname + "/public")));
 // app.use('/]public', express.static(path.join(__dirname, "public")));
-app.listen(port, console.log(`server running on port ${port}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running on port ${PORT}`));
